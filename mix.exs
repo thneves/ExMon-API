58db476fd2a8d238f65934bbcba5c61e9d2f3447
thales-pinner <thales.neves@pinpeople.com.br>
2022-08-23 21:30:52 -0300

    Add encryption to password

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule ExMonApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:argon2_elixir, "~>2.0"}
     ]
   end
 
