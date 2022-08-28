cfe2c7ee32a0b96c58ef1e5cf183fdd26a43378a
thales-pinner <thales.neves@pinpeople.com.br>
2022-08-28 15:28:06 -0300

    Add fallback controller for error handling

--- lib/ex_mon_api_web/controllers/trainers_controller.ex
+++ lib/ex_mon_api_web/controllers/trainers_controller.ex
@@ -1,6 +1,8 @@
 defmodule ExMonApiWeb.TrainersController do
   use ExMonApiWeb, :controller
 
+  action_fallback ExMonApi.FallbackController
+
   def create(conn, params) do #receives action params, call create_trainer and handle the response
     params
     |> ExMonApi.create_trainer()
@@ -9,7 +11,9 @@ defmodule ExMonApiWeb.TrainersController do
 
   defp handle_response({:ok, trainer}, conn) do
     conn
-    |> put_status(:ok)
+    |> put_status(:created)
     |> render("create.json", trainer: trainer)
   end
+
+  defp handle_response({:error, _changeset} = error, _conn), do: error
 end
---
+++ lib/ex_mon_api_web/views/trainers_view.ex
@@ -0,0 +1,16 @@
+defmodule ExMonApiWeb.TrainersView do
+  use ExMonApiWeb, :view
+
+  alias ExMonApi.Trainer
+
+  def render("create.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at }}) do
+    %{
+      message: "Trainer Created!",
+      trainer: %{
+        id: id,
+        name: name,
+        inserted_at: inserted_at
+      }
+    }
+  end
+end
